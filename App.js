6b68392ddc5a7add8fdd43a5bc29a1403be8379e
steffanciprian <steffanciprian@gmail.com>
2021-04-07 21:59:03 +0300

    added fonts and AppLoading Component

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import * as Font from 'expo-font';
+import {AppLoading} from 'expo ';
 
+const fetchFonts = () => {
+    Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    return !fontLoaded ?
+        <AppLoading
+            startAsync={fetchFonts}
+            onFinish={() => setFontLoaded(true)}/>
+        : null;
+
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+        </View>
+    );
 }
