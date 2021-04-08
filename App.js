9e0fc312a23b17fbd0ee8f40a6d21e67830ae405
steffanciprian <steffanciprian@gmail.com>
2021-04-08 09:09:23 +0300

    resolved all errors and created stackContainerScreen

--- App.js
+++ App.js
@@ -1,30 +1,37 @@
-import {StatusBar} from 'expo-status-bar';
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
-import {AppLoading} from 'expo ';
+import MealsNavigator from "./Navigation/MealsNavigator";
 
 const fetchFonts = () => {
     Font.loadAsync({
-        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
         'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    });
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    })
 };
 
 export default function App() {
 
     const [fontLoaded, setFontLoaded] = useState(false);
 
-    return !fontLoaded ?
-        <AppLoading
+    if (!fontLoaded) {
+        return <AppLoading
             startAsync={fetchFonts}
-            onFinish={() => setFontLoaded(true)}/>
-        : null;
+            onFinish={() => setFontLoaded(true)}
+            onError={(err) => console.log(err)}/>
+    }
 
 
     return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <MealsNavigator/>
     );
 }
+const styles = StyleSheet.create({
+    screen:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+    }
+})
+
