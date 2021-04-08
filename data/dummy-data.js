ef8e4f649a8996ec61816c7b6086b19a8d05ad0d
steffanciprian <steffanciprian@gmail.com>
2021-04-08 13:12:02 +0300

    RecipeApp1.4 // Added navigation header

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+    new Category('c1','Italian','red'),
+    new Category('c2','Quick','blue'),
+    new Category('c3','Easy','yellow'),
+    new Category('c4','German','white'),
+    new Category('c5','Light','black'),
+    new Category('c6','Exotic','green'),
+    new Category('c7','Lovely','pink'),
+    new Category('c8','Breakfast','grey'),
+    new Category('c9','French','cadetblue'),
+    new Category('c10','Summer','red'),
+];
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Button} from "react-native";
+import {Button, Platform} from "react-native";
 
 const MealDetailScreen = props => {
     return (
+
         <View style={styles.screen}>
             <Text> The categories screen</Text>
             <Button title="Go Back To Categories"
-                    onPress={()=>{
-                props.navigation.popToTop();
-            }}/>
+                    onPress={() => {
+                        Platform.OS === 'ios' ?
+                            props.navigation.popToTop() :
+                            props.navigation.goBack();
+                    }}/>
         </View>
     )
 };
